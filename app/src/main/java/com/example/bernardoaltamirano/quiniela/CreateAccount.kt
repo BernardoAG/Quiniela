52e1a860b70510eb227909e6f081792cdc6ce54e
Bernardo <berny.ag30@gmail.com>
2018-05-08 13:02:50 -0500

    a

---
+++ app/src/main/java/com/example/bernardoaltamirano/quiniela/CreateAccount.kt
@@ -0,0 +1,12 @@
+package com.example.bernardoaltamirano.quiniela
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateAccount : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_account)
+    }
+}
