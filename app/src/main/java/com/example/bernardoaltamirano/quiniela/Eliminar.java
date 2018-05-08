725b381f5e7a560251e14551c99cb9e7edc99060
Bernardo <berny.ag30@gmail.com>
2018-05-08 10:15:17 -0500

    sql

--- app/src/main/java/com/example/bernardoaltamirano/quiniela/Eliminar.java
+++ app/src/main/java/com/example/bernardoaltamirano/quiniela/Eliminar.java
@@ -2,12 +2,18 @@ package com.example.bernardoaltamirano.quiniela;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class Eliminar extends AppCompatActivity {
+
+    private EditText mail, psw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eliminar);
+        mail = (EditText) findViewById(R.id.txMail);
+        psw = (EditText) findViewById(R.id.txPsw);
+
     }
 }
